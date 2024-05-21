0dfa2f747841b05373c5240d9584fb72f728bd01
BlastTea <arya.jbr.999@gmail.com>
2024-05-21 09:21:34 +0700

    feat: setup project

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:m_widget/m_widget.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  MWidget.initialize(
+    defaultLanguage: LanguageType.indonesiaIndonesian,
+    defaultTheme: ThemeValue(
+      themeMode: ThemeMode.dark,
+      color: const Color(0xFF6A2C70),
+      withCustomColors: true,
+      useDynamicColors: true,
+    ),
+  );
+
   runApp(const MainApp());
 }
 
@@ -8,13 +21,20 @@ class MainApp extends StatelessWidget {
   const MainApp({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
-    );
-  }
+  Widget build(BuildContext context) => MWidgetDynamicColorBuilder(
+        builder: (context, theme, darkTheme, themeMode, colorScheme) {
+          ColorScheme? effectiveColorScheme = colorScheme?.copyWith(surface: colorScheme.primary);
+
+          return MaterialApp(
+            theme: theme.copyWith(colorScheme: effectiveColorScheme),
+            darkTheme: darkTheme.copyWith(colorScheme: effectiveColorScheme),
+            themeMode: themeMode,
+            home: Scaffold(
+              body: Center(
+                child: Text('Hello World!'),
+              ),
+            ),
+          );
+        },
+      );
 }
